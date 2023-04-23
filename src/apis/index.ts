dfdb5168431f7adf7a22c3f214a6ffbe008f752a
lu <luqiangbovip@163.com>
2023-04-24 02:17:35 +0800

    gitee

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,4 +1,5 @@
 import request from '@/utils/request'
 
 // 新闻列表
-export const fetchGitee123 = () => request({ url: `https://caniuse.gitee.io/123.json`, payload: {} })
+export const fetchGitee123 = () =>
+  request({ url: `https://gitee.com/caniuse/caniuse/raw/main/json/123.json`, payload: {} })
