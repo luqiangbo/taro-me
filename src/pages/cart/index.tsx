7c70da8d1074e42e09329bdf63e4ba8fdeb2a66e
lu <luqiangbovip@163.com>
2023-07-24 00:45:44 +0800

    总价

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,14 +1,14 @@
 import { useDidShow } from '@tarojs/taro'
-import { useSetState } from 'ahooks'
-import { find, cloneDeep } from 'lodash-es'
-import { Checkbox, Button, Image, Price, InputNumber } from '@nutui/nutui-react-taro'
+import { useSetState, useUpdateEffect } from 'ahooks'
+import { Checkbox, Button, Image, Price, InputNumber, Popup, Radio } from '@nutui/nutui-react-taro'
 import { useSnapshot } from 'valtio'
-import { filter } from 'lodash-es'
+import { filter, find, cloneDeep } from 'lodash-es'
 
 import CAll from '@/components/all_comp'
 import CTabber from '@/components/tabbar_comp'
-import { mUser } from '@/store'
-import { fetchSpuCart } from '@/apis'
+import { mUser, mCommon } from '@/store'
+import { fetchSpuCart, fetchAddressList } from '@/apis'
+import { add, multiply } from '@/utils'
 
 definePageConfig({
   navigationBarTitleText: '购物车',
@@ -18,14 +18,40 @@ export default function EssayPage() {
   const snapUser = useSnapshot(mUser)
   const [state, setState] = useSetState({
     mainList: [],
+    isAdministration: false,
+    addressList: [],
+    addressActive: {},
+    isOpenAddress: false,
+    skuIdListActive: [],
+    skuIdList: [],
+    isAllSkuId: false,
+    skuPriceObj: {},
+    priceActive: '0',
   })
 
   useDidShow(() => {
     init()
   })
 
+  useUpdateEffect(() => {
+    onPrice()
+  }, [JSON.stringify(state.skuIdListActive)])
+
   const init = () => {
     onFetchSpuCart()
+    onFetchAddressList()
+  }
+
+  const onPrice = () => {
+    const localSku = mUser.cart
+    const { skuPriceObj, skuIdListActive } = state
+    let priceActive = '0'
+    skuIdListActive.forEach((id) => {
+      priceActive = add(priceActive, multiply(skuPriceObj[id], find(localSku, { id: id }).quantity))
+    })
+    setState({
+      priceActive,
+    })
   }
 
   const onFetchSpuCart = async () => {
@@ -36,13 +62,17 @@ export default function EssayPage() {
     }
     const [err, res] = await fetchSpuCart(req)
     if (err) return
-
+    let skuIdList = []
+    const skuPriceObj = {}
     const resTrim = res.list.map((u) => {
       let sku = []
+
       console.log({ u })
       u.sku.forEach((h) => {
         const sole = find(localSkuList, { id: h.id })
         if (sole) {
+          skuIdList.push(h.id)
+          skuPriceObj[h.id] = h.price
           sku.push({
             ...h,
             quantity: sole.quantity,
@@ -56,7 +86,25 @@ export default function EssayPage() {
     })
     setState({
       mainList: resTrim,
+      skuIdList,
+      skuPriceObj,
     })
+  }
+
+  const onFetchAddressList = async () => {
+    const req = {
+      current: 1,
+      pageSize: 100,
+      customId: snapUser.custom?.id,
+    }
+    const [err, res] = await fetchAddressList(req)
+    if (err) return
+    if (res.list.length) {
+      setState({
+        addressList: res.list,
+        addressActive: res.list[0],
+      })
+    }
   }
 
   const onSetCart = (skuId, v) => {
@@ -70,8 +118,87 @@ export default function EssayPage() {
     <div className="page-c page-cat">
       <CAll />
       <CTabber />
+      <Popup
+        visible={state.isOpenAddress}
+        style={{ height: '70%' }}
+        position="bottom"
+        round
+        onClose={() => {
+          setState({
+            isOpenAddress: false,
+          })
+        }}
+      >
+        <div className="px-4 py-8">
+          <Radio.Group
+            defaultValue={state.addressActive.id}
+            onChange={(id) => {
+              const sole = find(state.addressList, { id: id })
+              setState({
+                addressActive: sole,
+                isOpenAddress: false,
+              })
+            }}
+          >
+            {state.addressList.map((u) => (
+              <div className="flex items-center py-3">
+                <div className="w-v10 flex items-center justify-center">
+                  <Radio value={u.id}></Radio>
+                </div>
+                <div>
+                  <div>
+                    {u.receiver}-{u.phone}
+                  </div>
+                  <div className="text-gray-400 text-sm">
+                    <div>{u.summary}</div>
+                    <div>{u.detail}</div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Radio.Group>
+        </div>
+      </Popup>
+      <div className="fixed top-0 left-0 w-full bg-white h-v8">
+        <div className="px-3 flex justify-between items-center">
+          <div
+            className="flex-1 flex items-center text-sm text-gray-400"
+            onClick={() => {
+              if (state.addressList.length) {
+                setState({
+                  isOpenAddress: true,
+                })
+              } else {
+                mCommon.onToast('请添加地址')
+              }
+            }}
+          >
+            <div>地址:</div>
+            <div>{state.addressActive.id ? state.addressActive.summary : '无'}</div>
+          </div>
+          <div
+            className=""
+            onClick={() => {
+              setState({
+                isAdministration: !state.isAdministration,
+              })
+            }}
+          >
+            {state.isAdministration ? <div className="text-red-500"> 退出管理</div> : '管理'}
+          </div>
+        </div>
+      </div>
+      <div className="h-v8"></div>
       <div className="p-3">
-        <Checkbox.Group>
+        <Checkbox.Group
+          value={state.skuIdListActive}
+          onChange={(v) => {
+            setState({
+              skuIdListActive: v,
+              isAllSkuId: v.length === state.skuIdList.length,
+            })
+          }}
+        >
           {state.mainList.length ? (
             state.mainList.map((u) => (
               <div key={u.id} className="mb-2 overflow-hidden rounded-lg bg-white">
@@ -99,11 +226,11 @@ export default function EssayPage() {
                             <div className="flex-1">{h.name}</div>
                             <div>
                               <InputNumber
-                                value={h.quantity}
-                                min={1}
+                                defaultValue={h.quantity}
+                                min={0}
                                 max={h.inventory}
                                 onChange={(v) => {
-                                  onSetCart(u.id, v)
+                                  onSetCart(h.id, v)
                                 }}
                               />
                             </div>
@@ -131,14 +258,34 @@ export default function EssayPage() {
       <div className="safe-area fixed left-0 w-full  bg-white" style={{ bottom: '50px' }}>
         <div className="flex justify-between items-center p-2 h-v15">
           <div>
-            <Checkbox> 全选</Checkbox>
+            <Checkbox
+              checked={state.isAllSkuId}
+              onChange={(v) => {
+                setState({
+                  isAllSkuId: v,
+                  skuIdListActive: v ? state.skuIdList : [],
+                })
+              }}
+            >
+              全选
+            </Checkbox>
           </div>
-          <div className="flex items-center">
-            <div className="mr-2">总价</div>
-            <Button className="w-v20" color="linear-gradient(to right, #5bae6f, #249543)">
-              结算
-            </Button>
-          </div>
+          {state.isAdministration ? (
+            <div>
+              <Button className="w-v20" color="linear-gradient(to right, #cf471e, #e24e58)">
+                删除
+              </Button>
+            </div>
+          ) : (
+            <div className="flex items-center">
+              <div className="mr-2">
+                总价: <Price price={state.priceActive} size="large"></Price>
+              </div>
+              <Button className="w-v20" color="linear-gradient(to right, #5bae6f, #249543)">
+                结算
+              </Button>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 import { mapValues } from 'lodash-es'
 import Taro, { getCurrentInstance } from '@tarojs/taro'
 import qs from 'qs'
+import Decimal from 'decimal.js'
 
 export const to = (promise) => promise.then((res) => [null, res]).catch((err) => [err, null])
 
@@ -16,4 +17,24 @@ export const goto = ({ url, data = {} }) => {
   Taro.navigateTo({
     url: `${url}?${qs.stringify(data)}`,
   })
+}
+
+// 加
+export function add(num1, num2) {
+  return new Decimal(num1).plus(num2).toString()
+}
+
+// 减
+export function subtract(num1, num2) {
+  return new Decimal(num1).minus(num2).toString()
+}
+
+// 乘
+export function multiply(num1, num2) {
+  return new Decimal(num1).times(num2).toString()
+}
+
+// 除
+export function divide(num1, num2) {
+  return new Decimal(num1).dividedBy(num2).toString()
 }
